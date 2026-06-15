5107d8e19857e269ad0f1bd4026657fe0480fc49
Oumar11bah <obah11105@gmail.com>
2026-06-15 10:39:46 +0000

    debug: endpoint diagnostic + staticfiles dirs

--- GestionEcole/settings.py
+++ GestionEcole/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'GestionEcole.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build'), os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build'), os.path.join(BASE_DIR, 'staticfiles'), os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
